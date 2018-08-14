3294a40df06033ff7d1adcb92537d3ad8e804be8
hamza192 <41551916+hamza192@users.noreply.github.com>
2018-08-14 05:20:13 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -97,27 +97,28 @@ client.on('message', message => {
 
 client.on('message', message => {
     if (message.content.startsWith("رابط")) {
-if(!message.channel.guild) return;
-
         message.channel.createInvite({
         thing: true,
         maxUses: 1,
-        maxAge: 86400
+        maxAge: 3600,
     }).then(invite =>
       message.author.sendMessage(invite.url)
     )
     const embed = new Discord.RichEmbed()
         .setColor("RANDOM")
-        .setDescription(`  في الخاص ${message.guild.name}تم ارسالك رابط سيرفر `)
-   .setFooter("اسم السيرفر","رابط صوره السيرفر")
-      message.channel.sendEmbed(embed).then(message => {message.delete(3000)})
-              const EmbedMalek = new Discord.RichEmbed()
-        .setColor("000000")
-        .setDescription(`هذا الرابط لخمسه مستخدمين فقط لمده اربعه وعشرين ساعه${message.guild.name} رابط سيرفر  `)
-        .setFooter("اسم السيرفر","رابط صوره السيرفر")
-      message.author.sendEmbed(EmbedMalek)
+          .setDescription(" تم أرسال الرابط برسالة خاصة ")
+           .setAuthor(client.user.username, client.user.avatarURL)
+                 .setAuthor(client.user.username, client.user.avatarURL)
+                .setFooter('طلب بواسطة: ' + message.author.tag)
+
+      message.channel.sendEmbed(embed).then(message => {message.delete(10000)})
+              const Embed11 = new Discord.RichEmbed()
+        .setColor("RANDOM")
+        
+    .setDescription(" مدة الرابط : ساعه  عدد استخدامات الرابط : 1 ")
+      message.author.sendEmbed(Embed11)
     }
-});
+}); 
 
 
 
