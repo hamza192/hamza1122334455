bc059dccd9bd8c1ee2756e92f2122071fd1d1765
hamza192 <41551916+hamza192@users.noreply.github.com>
2018-08-14 04:45:20 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,7 +12,29 @@ client.on('message', message => {
 });
 
 
+client.on('message', message => {
+    if (message.content.startsWith("رابط")) {
+if(!message.channel.guild) return;
 
+        message.channel.createInvite({
+        thing: true,
+        maxUses: 1,
+        maxAge: 86400
+    }).then(invite =>
+      message.author.sendMessage(invite.url)
+    )
+    const embed = new Discord.RichEmbed()
+        .setColor("RANDOM")
+        .setDescription(`  في الخاص ${message.guild.name}تم ارسالك رابط سيرفر `)
+   .setFooter("اسم السيرفر","رابط صوره السيرفر")
+      message.channel.sendEmbed(embed).then(message => {message.delete(3000)})
+              const EmbedMalek = new Discord.RichEmbed()
+        .setColor("000000")
+        .setDescription(`هذا الرابط لخمسه مستخدمين فقط لمده اربعه وعشرين ساعه${message.guild.name} رابط سيرفر  `)
+        .setFooter("اسم السيرفر","رابط صوره السيرفر")
+      message.author.sendEmbed(EmbedMalek)
+    }
+});
 
 
 
