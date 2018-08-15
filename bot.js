bc543d5f9d3e85c36a6e89dfb6977ec191318312
hamza192 <41551916+hamza192@users.noreply.github.com>
2018-08-15 13:46:14 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -455,55 +455,6 @@ client.on('message', (message) => {
 
 
 
-client.on("guildMemberAdd", (member) => {
-
-    let channel = member.guild.channels.get("450053252222812162");
-
-    if (!channel) {
-
-        console.log("!the channel id it's not correct");
-
-        return;
-
-    }
-
-    if (member.id == client.user.id) {
-
-        return;
-
-    }
-
-    console.log('-');
-
-    var guild;
-
-    while (!guild)
-
-        guild = client.guilds.get("230681753965166592");
-
-    guild.fetchInvites().then((data) => {
-
-        data.forEach((Invite, key, map) => {
-
-            var Inv = Invite.code;
-
-            if (dat[Inv])
-
-                if (dat[Inv] < Invite.uses) {
-
- channel.send(تم دعوته بواسطة  ${Invite.inviter}) ;
-
- }
-
-            dat[Inv] = Invite.uses;
-
-
-
-       });
-
-    });
-
-});
 
 
 
