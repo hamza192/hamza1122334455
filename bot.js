eb93b42c36f2779e80bd09bdf5b98ac0dd12cdb6
hamza192 <41551916+hamza192@users.noreply.github.com>
2018-08-15 13:36:20 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -457,7 +457,21 @@ client.on('message', (message) => {
 
 
 
-
+client.on('message', message => {
+   if(message.content.startsWith(prefix + "دعواتي")) {
+    message.guild.fetchInvites().then(invs => {
+      let user = message.mentions.users.first() || message.author
+      let personalInvites = invs.filter(i => i.inviter.id === user.id);
+      let inviteCount = personalInvites.reduce((p, v) => v.uses + p, 0);
+               let mmmmEmbed = new Discord.RichEmbed()
+                         .setAuthor(client.user.username)
+                         .setThumbnail(message.author.avatarURL)
+ .addField(لقد قمت بدعوة :, ${inviteCount})
+           .setFooter(- Requested By: ${message.author.tag});
+           message.channel.send(mmmmEmbed)
+});
+  }
+});
 
 
 
