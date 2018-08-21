af33f77f75c0209dee60428c4ea8afd79f0e6e2a
hamza192 <41551916+hamza192@users.noreply.github.com>
2018-08-21 10:35:06 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1067,7 +1067,7 @@ client.on('message', message => {
 
 
 client.on('message',async message => {
-  if(message.content.startsWith(prefix + "setvoice")) {
+  if(message.content.startsWith(prefix + "صوت")) {
   if(!message.guild.member(message.author).hasPermissions('MANAGE_CHANNELS')) return message.reply('❌ **ليس لديك الصلاحيات الكافية**');
   if(!message.guild.member(client.user).hasPermissions(['MANAGE_CHANNELS','MANAGE_ROLES_OR_PERMISSIONS'])) return message.reply('❌ **ليس معي الصلاحيات الكافية**');
   message.channel.send('✅| **تم عمل الروم بنجاح**');
